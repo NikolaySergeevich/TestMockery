ede1f1d634d96f2a2582612ece5c36eec1f36d7d
Nikolay <nik.pavlovskiy.1997@mail.ru>
2024-05-16 09:56:16 +0300

    add mock.On

--- suncalc/calc_test.go
+++ suncalc/calc_test.go
@@ -2,9 +2,16 @@ package suncalc_test
 
 import (
 	"Lesson/suncalc"
+	"Lesson/suncalc/mocks"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test(t *testing.T) {
-	rise, set := suncalc.Calc(nil, "Moscow")//вот тут вместо nil и нужно вставлять mock
+	mock := mocks.NewGeocoder(t)
+	mock.On("GetCoordsByName", "Moscow").Return(55.7558, 37.6176, nil)
+	rise, set := suncalc.Calc(mock, "Moscow")//вот тут вместо nil и нужно вставлять mock
+	assert.Equal(t, "2000-01-01 05:59:15 +0000 UTC", rise.String())
+	assert.Equal(t, "2000-01-01 13:06:05 +0000 UTC", set.String())
 }
